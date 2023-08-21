9ea7ade5a892b718d5adc555519540422450b44a
Indra Zen <indrapranata120@gmail.com>
2023-08-21 12:38:37 +0800

    Main styles

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,3 +7,13 @@ import Sidebar from '@/components/Sidebar.vue'
     <Sidebar />
   </div>
 </template>
+
+<style scoped>
+.container {
+  display: grid;
+  width: 96%;
+  margin: 0 auto;
+  gap: 1.8rem;
+  grid-template-columns: 12rem auto 23rem;
+}
+</style>
