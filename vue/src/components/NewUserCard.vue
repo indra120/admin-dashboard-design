6ff0adea0b2eb145fb978e5ab3172dccdead79cb
Indra Zen <indrapranata120@gmail.com>
2023-08-22 14:25:41 +0800

    New-users section styles

--- vue/src/components/NewUserCard.vue
+++ vue/src/components/NewUserCard.vue
@@ -13,3 +13,19 @@ defineProps({
     <p>{{ caption }}</p>
   </div>
 </template>
+
+<style scoped>
+.user-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+img {
+  width: 5rem;
+  height: 5rem;
+  margin-bottom: 0.4rem;
+  border-radius: 50%;
+}
+</style>
