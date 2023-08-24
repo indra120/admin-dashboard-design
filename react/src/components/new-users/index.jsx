ecb85df6533ea278176fce851863af3cc2662a75
Indra Zen <indrapranata120@gmail.com>
2023-08-24 17:18:55 +0800

    Tailwind integration #5

--- react/src/components/new-users/index.jsx
+++ react/src/components/new-users/index.jsx
@@ -1,11 +1,10 @@
 import { User } from './user'
-import styles from '@/styles/new-users.module.scss'
 
 export function NewUsers() {
   return (
-    <div className={styles.newUsers}>
+    <div className='new-users $-mt-[1.3rem]'>
       <h2>New Users</h2>
-      <div className={styles.userList}>
+      <div className='user-list $-bg-white $-shadow-[0_2rem_3rem_rgba(132,139,200,0.18)] $-flex $-justify-around $-flex-wrap $-gap-[1.4rem] $-cursor-pointer $-transition-all $-duration-[0.3s] $-ease-[ease] $-mt-4 $-p-[1.8rem] $-rounded-[2rem]'>
         {users.map((user, i) => <User key={i} {...user} />)}
 
         <User image="images/plus.png" name="More" caption="New User" />
--- react/src/components/new-users/user.jsx
+++ react/src/components/new-users/user.jsx
@@ -1,5 +1,3 @@
-import styles from '@/styles/new-users.module.scss'
-
 /**
  * @param {Object} props
  * @param {string} props.image
@@ -8,8 +6,8 @@ import styles from '@/styles/new-users.module.scss'
  */
 export function User({ image, name, caption, ...props }) {
   return (
-    <div className={styles.user} {...props}>
-      <img src={image} />
+    <div className='user $-flex $-flex-col $-items-center $-justify-center' {...props}>
+      <img src={image} className='$-w-20 $-h-20 $-mb-[0.4rem] $-rounded-full' />
       <h2>{name}</h2>
       <p>{caption}</p>
     </div>
